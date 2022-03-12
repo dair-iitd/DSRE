bc025f2d097c510799e44ae3972d597c9757d9fe
rathorevipul28 <rathorevipul28@gmail.com>
2022-03-12 12:46:20 +0530

    Update train_nyt10m.sh

--- train_nyt10m.sh
+++ train_nyt10m.sh
@@ -5,10 +5,11 @@ train_file='benchmark/nyt10m/nyt10m_train.txt'
 val_file='benchmark/nyt10m/nyt10m_val.txt'
 test_file='benchmark/nyt10m/nyt10m_test.txt'
 rel2id_file='benchmark/nyt10m/nyt10m_rel2id.json'
-batch_size=16
+batch_size=32
 max_length=512
 max_epoch=5
+lr=1e-5
 seed=772
 name=$pretrain_path'_'$max_length'_'$batch_size'_'$max_epoch'_'$seed'_nyt10m'
 echo $name
-python main.py --pretrain_path $pretrain_path --train_file $train_file --val_file $val_file --test_file $test_file --rel2id_file $rel2id_file --batch_size $batch_size --max_epoch $max_epoch --max_length $max_length --ckpt $name --devs 0
+python main.py --pretrain_path $pretrain_path --train_file $train_file --val_file $val_file --test_file $test_file --rel2id_file $rel2id_file --batch_size $batch_size --lr $lr --max_epoch $max_epoch --max_length $max_length --ckpt $name --devs 0
